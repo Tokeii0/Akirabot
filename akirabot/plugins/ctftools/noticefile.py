74db4e8d54eb99aa68c529b03f55a85b5d5a2c6e
猫捉鱼 <111427585+Tokeii0@users.noreply.github.com>
2022-11-09 13:38:24 +0800

    🫠 新增判断防止远程执行

--- akirabot/plugins/ctftools/noticefile.py
+++ akirabot/plugins/ctftools/noticefile.py
@@ -6,7 +6,7 @@ from nonebot.matcher import Matcher
 from nonebot.params import CommandStart,EventType,EventMessage,CommandArg,Arg
 from nonebot.permission import SUPERUSER
 from nonebot.typing import T_State
-import json
+import json,re
 import akirabot.plugins.ctftools.tokeii.Tokeiictftools as tkts
 noticefile = on_notice()
 @noticefile.handle()
@@ -14,9 +14,12 @@ async def _(bot: Bot,event: NoticeEvent,state: T_State):
     if event.notice_type == 'offline_file':
         #文件名
         filename,url,userid = event.file['name'],event.file['url'],event.user_id
-        print(filename,url,userid)
-        await noticefile.send(f'[+] 文件接收成功如要使用请记录好文件名\n[+] 文件名为:{filename}\n[+] 文件大小为{event.file["size"]}')
-        await noticefile.finish(tkts.Tokeiictftools().downloadfile(filename,url,userid))
+        if re.match(r'^[0-9a-zA-Z.]+$',filename):
+            print(filename,url,userid)
+            await noticefile.send(f'[+] 文件接收成功如要使用请记录好文件名\n[+] 文件名为:{filename}\n[+] 文件大小为{event.file["size"]}')
+            await noticefile.finish(tkts.Tokeiictftools().downloadfile(filename,url,userid))
+        else:
+            await noticefile.finish(f'[-] 文件名只允许含有[0-9a-zA-Z.],请重新发送')
 
 
 usergetfile = on_command('查看文件',priority=1, block=False)
