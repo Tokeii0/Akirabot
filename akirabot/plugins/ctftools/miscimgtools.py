78ada53044b33688b36179fab609990e93918a3d
猫捉鱼 <111427585+Tokeii0@users.noreply.github.com>
2022-11-13 14:51:52 +0800

    🫠 新增调用QQocrapi

--- akirabot/plugins/ctftools/miscimgtools.py
+++ akirabot/plugins/ctftools/miscimgtools.py
@@ -19,6 +19,12 @@ async def _(bot: Bot,event: MessageEvent,args: Message=CommandArg()):
             filename = tkts.Tokeiictftools().getoriginimg(msg,event.user_id)
             #print(filename)
             await bot.call_api("upload_private_file",user_id=event.user_id, file=filename,name="output.png")
+            imgid = await fixpng.send(Message(f'[CQ:image,file=file:///{filename}]'))
+            # imgid = imgid['message_id']
+            # print(imgid)
+            # print(await bot.call_api("get_msg",message_id=imgid))
+            #ocr
+            #print(await bot.call_api('ocr_image',image=filename))
         except Exception as e:
             await fixpng.finish(f'[-] 修复失败,错误信息:{e}')
             
@@ -35,7 +41,7 @@ async def _(bot: Bot,event: MessageEvent,args: Message=CommandArg()):
             pass
 
 
-        
+
 jsteg = on_command('jsteg',aliases={'jsteg'},priority=1, block=False)
 @jsteg.handle()
 async def _(bot: Bot,event: MessageEvent,args: Message=CommandArg()):
@@ -49,3 +55,34 @@ async def _(bot: Bot,event: MessageEvent,args: Message=CommandArg()):
         except Exception as e:
             print(e)
             #await jsteg.finish(f'[-] 解码失败,错误信息:{e}')
+picocr = on_command('picocr',aliases={'ocr'}, priority=5)
+#checkyml = yaml.safe_load(open('pluginconfig.yml','r+',encoding='utf-8'))['picocr']['status']
+@picocr.handle()
+async def handle_first_receive(event: MessageEvent, state: T_State, picocr: Message = CommandArg()):
+    if picocr:
+        state['picocr'] = picocr
+@picocr.got("picocr",prompt="快发图")
+async def get_picocr(bot: Bot,event: MessageEvent,msg: Message = Arg("picocr")):
+    try:
+        if msg[0].type == "image":
+            imageid = str(msg[0]).split(',')[1].split('=')[1]
+            ocrres = await bot.call_api('ocr_image', image=imageid)
+            print(ocrres)
+            result = ''
+            for i in range(len(ocrres['texts'])):
+                confidence=str(ocrres['texts'][i]['confidence'])
+                text = str(ocrres['texts'][i]['text'])
+                result +=  text+'('+confidence+'%)\n'
+                if 'flag' in result :
+                    if '[' in result or ']' in result:
+                        resultnew = result.replace('[','{').replace(']','}')
+                        await picocr.send(Message('[+] 图片识别内容为:\n'+result+'[+] 智能修改后为:\n'+resultnew.replace(confidence+'%','更正后')))
+                else:
+                    await picocr.send(Message('[+] 图片识别内容为:\n'+result))    
+                    
+            
+            #('图片识别内容为'+ocrres['texts'][0]['text'],'准确率为'+str(ocrres['texts'][0]['confidence'])+'%')
+            await picocr.finish()
+    except Exception as e:
+        print(e)
+        return
